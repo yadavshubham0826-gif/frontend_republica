daadb291d1f4bd2bbe3d46bfd5f1be6c999d9bcc
Shubham Yadav <yadavshubham0826@gmail.com>
2026-01-26 01:26:02 +0530

    UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -470,6 +470,40 @@ const Home = () => {
       <span style={{ color: '#0077B5' }}>Republica DRC</span>
     </a>
   </strong>
+  </p>
+   <p>
+  <strong>
+    <a
+      
+      href="https://www.facebook.com/share/1MVv3pRqyQ/" // replace if needed
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{
+        display: 'inline-flex',
+        alignItems: 'center',
+        gap: '8px',
+        textDecoration: 'none',
+        fontWeight: '600',
+        fontSize: '18px',
+        color: '#1877F2', // Facebook blue
+      }}
+    >
+      {/* Facebook icon */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="22"
+        height="22"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        style={{ flexShrink: 0 }}
+      >
+        <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.326 24h11.495v-9.294H9.691V11.01h3.13V8.309c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.696h-3.12V24h6.116C23.403 24 24 23.403 24 22.674V1.326C24 .597 23.403 0 22.675 0z"/>
+      </svg>
+
+      {/* Facebook text */}
+      <span>Republica DRC</span>
+    </a>
+  </strong>
 </p>
  <p><strong>Address:</strong> Republica, Political Science Association<br />Daulat Ram College, University Of Delhi, 4 Maurice Nagar,New Delhi-110007</p>
                     </div>
@@ -486,7 +520,7 @@ const Home = () => {
                 </div>
               </div>
             </section>
-                </main>
+                           </main>
   );
 };
 
